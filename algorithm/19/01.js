62829586b0eb51a5ef658ab5603cafbc48d4bff8
HoHoRang <wisehero7@gmail.com>
2022-04-07 09:30:17 +0900

    19일차 알고리즘

---
+++ algorithm/19/01.js
@@ -0,0 +1,81 @@
+/*
+수포자는 수학을 포기한 사람의 준말입니다. 
+수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 
+수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 
+가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한 조건
+시험은 최대 10,000 문제로 구성되어있습니다.
+문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+*/
+function solution(answers) {
+  const temp = new Array(answers.length).fill(1);
+
+  const stu1 = temp.map((_, idx) => {
+    return (idx % 5) + 1;
+  });
+
+  const stu2 = temp.map((_, idx) => {
+    if (idx % 2 === 0) {
+      return 2;
+    } else if (idx % 8 === 1) {
+      return 1;
+    } else if (idx % 8 === 3) {
+      return 3;
+    } else if (idx % 8 === 5) {
+      return 4;
+    } else if (idx % 8 === 7) {
+      return 5;
+    }
+  });
+
+  const stu3 = temp.map((_, idx) => {
+    if (idx % 10 === 0 || idx % 10 === 1) {
+      return 3;
+    } else if (idx % 10 === 2 || idx % 10 === 3) {
+      return 1;
+    } else if (idx % 10 === 4 || idx % 10 === 5) {
+      return 2;
+    } else if (idx % 10 === 6 || idx % 10 === 7) {
+      return 4;
+    } else if (idx % 10 === 8 || idx % 10 === 9) {
+      return 5;
+    }
+  });
+
+  const count = {
+    stu1: 0,
+    stu2: 0,
+    stu3: 0,
+  };
+
+  for (let i = 0; i < answers.length; i++) {
+    if (answers[i] === stu1[i]) {
+      count.stu1++;
+    }
+    if (answers[i] === stu2[i]) {
+      count.stu2++;
+    }
+    if (answers[i] === stu3[i]) {
+      count.stu3++;
+    }
+  }
+
+  const max = Math.max(...Object.values(count));
+  const result = [];
+
+  for (let i = 1; i <= 3; i++) {
+    if (max === count[`stu${i}`]) {
+      result.push(i);
+    }
+  }
+
+  return result;
+}
