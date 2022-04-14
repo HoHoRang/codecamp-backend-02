45604ff4aeee6d143750dc8a41abf85d12fde398
HoHoRang <wisehero7@gmail.com>
2022-04-15 01:42:51 +0900

    main-project 수정

--- main-project/backend/src/apis/auth/owner.auth.module.ts
+++ main-project/backend/src/apis/auth/owner.auth.module.ts
@@ -2,9 +2,9 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { JwtRefreshStrategy } from 'src/commons/auth/jwt-refresh.strategy';
-import { JwtGoogleStrategy } from 'src/commons/auth/jwt-social-google.strategy';
-import { JwtKakaoStrategy } from 'src/commons/auth/jwt-social-kakao.strategy';
-import { JwtNaverStrategy } from 'src/commons/auth/jwt-social-naver.strategy';
+import { JwtGoogleOwnerStrategy } from 'src/commons/auth/jwt-social-google-owner.strategy';
+import { JwtKakaoOwnerStrategy } from 'src/commons/auth/jwt-social-kakao-owner.strategy';
+import { JwtNaverOwnerStrategy } from 'src/commons/auth/jwt-social-naver-owner.strategy';
 import { Owner } from '../owner/entities/owner.entity';
 import { OwnerService } from '../owner/owner.service';
 import { OwnerAuthController } from './owner.auth.controller';
@@ -18,9 +18,9 @@ import { OwnerAuthService } from './owner.auth.service';
   ],
   providers: [
     JwtRefreshStrategy,
-    JwtGoogleStrategy,
-    JwtNaverStrategy,
-    JwtKakaoStrategy,
+    JwtGoogleOwnerStrategy,
+    JwtNaverOwnerStrategy,
+    JwtKakaoOwnerStrategy,
     OwnerAuthResolver, //
     OwnerAuthService,
     OwnerService,
