1df3e5567e59487cf888cdb0ddc08da7dcc64e5b
HoHoRang <wisehero7@gmail.com>
2022-04-27 18:30:35 +0900

    33일차 수업

---
+++ class/33-02-msa-graphql/services/auth/src/app.resolver.ts
@@ -0,0 +1,17 @@
+import { AppService } from './app.service';
+import { Resolver, Mutation } from '@nestjs/graphql';
+
+@Resolver()
+export class AppResolver {
+  constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @Mutation(() => String)
+  login() {
+    return 'login 성공!';
+  }
+}
