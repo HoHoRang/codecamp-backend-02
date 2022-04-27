4467f17241bc1853b682a04dbc17f8f2eb94ed3a
HoHoRang <wisehero7@gmail.com>
2022-04-27 12:37:56 +0900

    33일차 알고리즘

--- algorithm/33/01.js
+++ algorithm/33/01.js
@@ -42,3 +42,38 @@ function solution(progresses, speeds) {
   console.log(result);
   return result;
 }
+
+// function solution(progresses, speeds) {
+//   const answer = [];
+//   let day = 0;
+
+//   for (let i = 0; i < progresses.length; i++) {
+//     // 100% 완성까지 며칠이 걸리는지
+//     const process = Math.ceil((100 - progresses[i]) / speeds[i]);
+
+//     if (process > day) {
+//       day = process;
+//       answer[answer.length] = 1;
+//     } else if (day >= process) {
+//       answer[answer.length - 1]++;
+//     }
+//   }
+//   return answer;
+// }
+
+// function solution(progresses, speeds) {
+//   let day = 0;
+
+//   const answer = progresses.reduce((acc, cur) => {
+//     const process = Math.ceil((100 - cur) / speeds[i]);
+
+//     if (process > day) {
+//       day = process;
+//       acc[acc.length] = 1;
+//     } else if (day >= process) {
+//       acc[acc.length - 1]++;
+//     }
+//     return acc;
+//   }, []);
+//   return answer;
+// }
