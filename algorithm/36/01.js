880ee10266b02eb35f87304405c3a262245a4222
HoHoRang <wisehero7@gmail.com>
2022-05-02 14:05:55 +0900

    36일차 알고리즘

--- algorithm/36/01.js
+++ algorithm/36/01.js
@@ -41,6 +41,47 @@ function solution(people, limit) {
   return boatCount;
 }
 
+function solution(people, limit) {
+  people.sort((a, b) => b - a);
+
+  let last = people.length - 1;
+
+  return people.reduce((acc, cur, i) => {
+    if (i <= last) {
+      const weight = limit - cur;
+      acc++;
+
+      if (weight >= people[last]) {
+        last--;
+      }
+    }
+
+    return acc;
+  }, 0);
+}
+
+// function solution(people, limit) {
+//   let answer = 0;
+//   people.sort((a, b) => b - a);
+
+//   let last = people.length - 1;
+//   for (let i = 0; i < people.length; i++) {
+//     const weight = limit - people[i];
+//     answer++;
+
+//     // 가벼운 사람의 몸무게가 보트에 수용할 수 있는 몸무게보다 작을 경우(태울 수 있는 경우)
+//     if (weight >= people[last]) {
+//       last--;
+//     }
+
+//     // 대기열에 아무도 없는 경우
+//     if (i >= last) {
+//       return answer;
+//     }
+//   }
+// }
+
+// 효율성 통과 X
 // function solution(people, limit) {
 //   // 내림차순 정렬
 //   people = people.sort((a, b) => b - a);
