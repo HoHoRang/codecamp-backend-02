58f269fce2bcc92ef0cec0725aed3e04662e2bda
HoHoRang <wisehero7@gmail.com>
2022-05-04 12:48:54 +0900

    38일차 알고리즘

--- algorithm/38/01.js
+++ algorithm/38/01.js
@@ -44,3 +44,53 @@ function solution(priorities, location) {
 
   return count;
 }
+
+// function solution(priorities, location) {
+//   const origin = priorities[location];
+//   priorities[location] = "a";
+
+//   let answer = 0;
+//   while (true) {
+//     const search = priorities.indexOf("a");
+//     priorities[search] = origin;
+//     const max = Math.max(...priorities);
+//     priorities[search] = "a";
+
+//     if (priorities[0] === "a") {
+//       if (origin === max) {
+//         return ++answer;
+//       }
+//     }
+
+//     if (priorities[0] !== max) {
+//       priorities.push(priorities[0]);
+//       priorities.shift();
+//     } else {
+//       priorities.shift();
+//       answer++;
+//     }
+//   }
+// }
+
+// function solution(priorities, location) {
+//   const origin = priorities[location];
+//   priorities[location] = "a";
+
+//   const recursion = (count) => {
+//     const search = priorities.indexOf("a");
+//     priorities[search] = origin;
+//     const max = Math.max(...priorities);
+//     priorities[search] = "a";
+
+//     if (priorities[0] === "a" && origin === max) {
+//       return ++count;
+//     }
+
+//     priorities[0] === max ? count++ : priorities.push(priorities[0]);
+//     priorities.shift();
+
+//     return recursion(count);
+//   };
+
+//   return recursion(0);
+// }
